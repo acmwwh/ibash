8e546df8b3143a5b1933f6419cadb5857e7bce3d
linfan <lf.lin0447@sansec.com.cn>
2024-08-28 14:58:54 +0800

    feat: add log util

---
+++ demo/log_test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+source ../util/log.sh
+
+LOG_LEVEL=1
+
+log_debug "This is a debug message."
+log_info "This is a info message."
+log_warn "This is a warn message."
+log_err "This is a error message."
+log_always "This is a always message."
